19b512fc927b604967ee4decd255957ee1cf1f15
dicky_ardiansyah <eigerdlamborghini.ardiansyah@gmail.com>
2023-05-14 11:39:36 +0700

    2D PlatformerTutorial v1.0

---
+++ level_1.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _input(event):
+	if event.is_action_pressed("return_to_main_menu"):
+		get_tree().change_scene_to_file("res://main_menu.tscn")
---
+++ main_menu.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://level_1.tscn")
